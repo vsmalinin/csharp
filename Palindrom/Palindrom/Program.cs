c46f6ddedd86ddd2ffded961bf095ce360877fc9
Vladimir Malinin <vladimir.malinin@gmail.com>
2021-08-22 18:43:47 +0500

    1

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static bool IsPalindrom(string str)
+        {
+            if (str == null) throw new ArgumentNullException("str");
+            str = str.ToLower().Replace(" ", string.Empty);
+            return IsPalindromInternal(str);
+        }
+
+
+        static bool IsPalindromInternal(string str)
+        {
+            if (str.Length == 1 || string.IsNullOrEmpty(str)) return true;
+            if (!str[0].Equals(str[str.Length - 1])) return false;
+            return IsPalindromInternal(str.Substring(1, str.Length - 2));
+        }
+
+        static void Main(string[] args)
+        {
+            // А роза упала на лапу Азора
+            // Лазер Боре хер обрезал
+
+            Console.WriteLine("Input your text (program will check your text on palindrom):");
+            var txt = Console.ReadLine();
+            if (IsPalindrom(txt))
+            {
+                Console.WriteLine($"{Environment.NewLine}Your text '{txt}' IS palindrom!");
+            }
+            else
+            {
+                Console.WriteLine($"{Environment.NewLine}Your text '{txt}' IS NOT palindrom!");
+            }
+            // Console.Write($"{Environment.NewLine}Press any key to exit...");
+            // Console.ReadKey(true);
+        }
+    }
+}
