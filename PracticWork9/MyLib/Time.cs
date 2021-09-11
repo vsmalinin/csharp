ba4e994551391e9ab4e0461200702b89c5d6e293
Vladimir Malinin <vladimir.malinin@gmail.com>
2021-09-11 11:46:12 +0500

    practic work 9

---
+++ PracticWork9/MyLib/Time.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Time
+    {
+        private int hours;
+        private int minutes;
+        private static int counter = 0;
+
+        public int Hours
+        {
+            get
+            {
+                return hours;
+            }
+            set
+            {
+                if (value > 23 || value < 0)
+                {
+                    Console.WriteLine("Часы должны быть между 0 и 23.");
+                }
+                else
+                {
+                    hours = value;
+                }
+            }
+        }
+        public int Minutes
+        {
+            get
+            {
+                return minutes;
+            }
+            set
+            {
+                if (value > 59 || value < 0)
+                {
+                    Console.WriteLine("Минуты должны быть между 0 и 59.");
+                }
+                else
+                {
+                    minutes = value;
+                }
+            }
+        }
+
+        public Time() : this(0, 0)
+        {
+
+        }
+        public Time(int hours, int minutes)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            counter++;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Hours} часов {Minutes} минут.");
+        }
+
+        public static void DisplayCounter()
+        {
+            Console.WriteLine($"Создано {counter} объект(-ов) класса Time.");
+        }
+
+        public Time AddingMinutes(int m)
+        {
+            if (this.Minutes + m < 59)
+            {
+                this.Minutes += m;
+            }
+            else
+            {
+                this.Hours++;
+                this.Minutes = this.Minutes + m - 60;
+            }
+            return this;
+        }
+
+    }
+}
---
+++ PracticWork9/PracticWork9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MyLib;
+
+namespace PracticWork9
+{
+    class Program
+    {
+        static Time AddMinutes(Time t, int m)
+        {
+            if (t.Minutes + m < 59)
+            {
+                t.Minutes += m;
+            }
+            else
+            {
+                t.Hours++;
+                t.Minutes = t.Minutes + m - 60;
+            }
+            return t;
+        }
+
+        static void Main(string[] args)
+        {
+            Time t1 = new();
+            Console.Write("Введите количество часов: ");
+            t1.Hours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество минут: ");
+            t1.Minutes = Convert.ToInt32(Console.ReadLine());
+            t1.DisplayInfo();
+            Console.WriteLine("Вызывая статическую функцию добавим 30 минут и получим:");
+            AddMinutes(t1, 30);
+            t1.DisplayInfo();
+            Console.WriteLine("Вызывая метод класса добавим еще 40 минут и получим:");
+            t1.AddingMinutes(40);
+            t1.DisplayInfo();
+            Time.DisplayCounter();
+
+            Console.WriteLine("Создадим новый объект класса через другой конструктор:");
+            Time t2 = new(12, 12);
+            t2.DisplayInfo();
+            Time.DisplayCounter();
+
+            Console.ReadKey();
+        }
+    }
+}
