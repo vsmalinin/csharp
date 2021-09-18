67c86edc8637d628b02bab0cf5b30339efdb20f9
Vladimir Malinin <vladimir.malinin@gmail.com>
2021-09-18 15:06:51 +0500

    1

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,14 +4,30 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
-            Console.ReadKey(true);
+            if (args.Length == 1)
+            {
+                int N = Int32.Parse(args[0]);
+                double sum = 0;
+                for (int i =1; i <= N; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}! Сумма чисел аргумента равна {sum}.");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}! Некорректный аргумент, не могу вычислить сумму.");
+                return 1;
+            }
+            //Console.Write($"{Environment.NewLine}Press any key to exit...");
+            //Console.ReadKey(true);
         }
     }
 }
